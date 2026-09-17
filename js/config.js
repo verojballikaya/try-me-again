bd33474bcab4a4641d662985d0cda79d41b90a88
verojballikaya <ballikayaveroj@gmail.com>
2026-09-16 23:27:07 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_o4kaxJQhW', // e.g. us-east-2_o4kaxJQhW
         userPoolClientId: 'mn098efgeovfisa32b79bsr3j', // e.g. mn098efgeovfisa32b79bsr3j
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
